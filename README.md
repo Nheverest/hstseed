e1da65745d4c0d476abfba185beeda667d2a3040
Hervé Stern <herve_stern@orange.fr>
2018-12-06 15:49:22 +0100

    test and Rest resources are running correctly using Undertow

---
+++ README.md
@@ -0,0 +1,4 @@
+* Test URLs
+- http://localhost:8081/rest/hello
+- http://localhost:8081/rest/hi/jsp
+- http://localhost:8081/rest/hi/ftl
--- src/test/java/com/hst/hsweb/interfaces/rest/HelloResourceIT.java
+++ src/test/java/com/hst/hsweb/interfaces/rest/HelloResourceIT.java
@@ -7,17 +7,25 @@ import io.restassured.response.Response;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.seedstack.seed.Configuration;
+import org.seedstack.seed.Logging;
 import org.seedstack.seed.testing.junit4.SeedITRunner;
 import org.seedstack.seed.undertow.LaunchWithUndertow;
+import org.slf4j.Logger;
 
 @RunWith(SeedITRunner.class)
 @LaunchWithUndertow
 public class HelloResourceIT {
+
+    @Logging
+    private Logger log;
+
     @Configuration("web.runtime.baseUrl")
     private String baseUrl;
 
     @Test
     public void testHelloWorld() throws Exception {
+        baseUrl += "rest/";
+        log.info("baseUrl: {}", baseUrl);
         Response response = given()
                 .auth().basic("demo", "demo")
                 .expect().statusCode(200)
